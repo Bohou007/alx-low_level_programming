8a4420318bb42f446c89a26106fb84f6f72c42d6
Bohou007 <bohoussoupaulelie@gmail.com>
2022-03-22 18:33:38 -0700

    Write a function that takes a pointer to an int as parameter and updates the value it points to to 98.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,7 @@
 
 /**
  * reset_to_98 - function to points to 98.
- * @n: parameter n 
-(* a blank line
- * Description: Longer description of the function)?
-(* section header: Section description)*
+ * @n:parameter n
  * Return: 98
  */
 void reset_to_98(int *n)
